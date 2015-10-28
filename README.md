192047f637a230657b882e035d3b8f0fc8c3b735
Alexander Lill <alexander.lill@tum.de>
2015-10-28 19:32:19 +0100

    Updates the dummydata and the readme

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # gnb
 Goliath National Bank - Group 12
+
+
+
+
+# Database Setup
+* Go into the directory of the project
+* Connect to the sql server:
+	mysql -u samurai -p
+* Drop the database, and import the schema and the dummydata:
+	drop database gnbdb; source database/gnbdb_create.sql ; source database/dummydata.sql ;
--- database/dummydata.sql
+++ database/dummydata.sql
@@ -1,8 +1,8 @@
-INSERT INTO user (first_name, last_name, email, pw_salt, pw_hash)
-VALUES ("Barney", "Stinson", "barney.stinson@gnb.com", "awesomesalt", "awesomehash");
+INSERT INTO user (first_name, last_name, email, pw_salt, pw_hash, role, approved_by_user_id, status)
+VALUES ("Barney", "Stinson", "barney.stinson@gnb.com", "awesomesalt", "awesomehash", 1, 1, 1);
 
-INSERT INTO user (first_name, last_name, email, pw_salt, pw_hash)
-VALUES ("Ted", "Mosby", "ted.mosby@tedmosbyisajerk.com", "awesomesalt2", "awesomehash2");
+INSERT INTO user (first_name, last_name, email, pw_salt, pw_hash, role, approved_by_user_id, status)
+VALUES ("Ted", "Mosby", "ted.mosby@tedmosbyisajerk.com", "awesomesalt2", "awesomehash2", 0, 1, 1);
 
 
 
@@ -46,5 +46,5 @@ VALUES ("AZU34HJ67ZT0JOT", 10000002);
 
 
 
-INSERT INTO transaction (source_account_id, destination_account_id, creation_timestamp, amount, description, tan_id, approved_at)
-VALUES (10000001, 10000002, "2015-10-01 15:00:00", 2000.00, "Take that money!", "A345GHJ67ZT7JOT", "2015-10-01 15:00:00");
+INSERT INTO transaction (source_account_id, destination_account_id, creation_timestamp, amount, description, tan_id, approved_at, approved_by_user_id)
+VALUES (10000001, 10000002, "2015-10-01 15:00:00", 2000.00, "Take that money!", "A345GHJ67ZT7JOT", "2015-10-01 15:00:00", 1);

cc0ccd16b6d268c17a83cc8dedf42029f39f5ae0
Mahmoud Naser <m.naser@tum.de>
2015-11-01 21:28:23 +0100

    verify_transaction basic debugging

--- project/accounts/verify_transaction.php
+++ project/accounts/verify_transaction.php
@@ -9,12 +9,19 @@ if (empty($_SESSION["user_id"]))
 if (empty($_SESSION["account_id"]))
 	die("Please choose an account");
 
-$account_id = $_SESSION["account_id"];
 
+$account_id 	= $_SESSION["account_id"];
+$role			= $_SESSION["role"] ; 
 $dest_code		= ( isset($_POST["dest_code"]) ? $_POST["dest_code"] : '' );
 $amount			= ( isset($_POST["amount"]) ? $_POST["amount"] : '' );
 $description	= ( isset($_POST["description"]) ? $_POST["description"] : '' );
 $tan_code		= ( isset($_POST["tan_code"]) ? $_POST["tan_code"] : '' );
+
+if ( isset($_POST["process"]) && $_POST["process"] == 'yes'){
+	if ( processTransaction($account_id, $dest_code, $amount, $description, $tan_code) == false)
+		die ("Unkonwn Transaction error, please connect our bros for help");
+	header("Location:".getPageURL($role));
+} 
 
 # verify all input is there 
 if (empty($account_id))
@@ -32,26 +39,41 @@ if (empty($tan_code))
 # no need for stage one 
 
 # Verify Operation
-$transaction_res = verifyTransaction($account_id, $dest_code, $amount , $description , $tan_code ) ; 
-#$transaction_res 	= array(
-#	'result'	=> false,
-#	'message'	=> '[TAN Code] Tan code has been used'
-#	) ; 
-
-#var_dump($transaction_res) ; 	
-	
+$transaction_res = verifyTransaction($account_id, $dest_code, $amount , $description , $tan_code ) ;  
 if ($transaction_res["result"] == true ){
 	
-	#  draw table with all further restuls and mark as verifed and provide button to confrim transaction
-	$r = 1 ; 
+	# Setting the summary line 
+	$summary 		= 'All details verified as of '.date("Y-m-d h:i:sa", $d)  ; 
+	## drawing headers
+	
+ 	echo '<table border="1">' 
+		. '<thead>'."\n"
+		. '<tr>'.'<th colspan="3">'.$summary.'</th>'."\n" 
+		. '</tr>'.'</thead>'."\n"
+		. '<tr><th>Destination</th>'.$dest_code.'</th></tr>' 
+		. '<tr><th>Amount</th>'.$amount.'</th></tr>' 
+		. '<tr><th>Description</th>'.$description.'</th></tr>' 
+		. '<tr><th>TAN Code</th>'.$tan_code.'</th></tr>' 
+		. '</table>' ; 
+	
+	echo 	'<form method="post" action="">'
+		.	'<input type="hidden" name="dest_code" value="'.$dest_code.'">'
+		.	'<input type="hidden" name="amount" value="'.$amount.'">'
+		.	'<input type="hidden" name="description" value="'.$description.'">'
+		.	'<input type="hidden" name="tan_code" value="'.$tan_code.'">'
+		.	'<input type="hidden" name="account_id" value="'.$account_id.'">'
+		.	'<input type="hidden" name="process" value="yes">'
+        .	'<input type="submit" value="Go Back">'
+		.	'</form>' ;	
+	
+	#echo "</table>"."\n" ;
+	#  draw table with all further restuls and mark as verifed and provide button to confrim transaction 
 } else {
-	$r = 2 ; 
 #	
 	$error_message 	= 'Transaction could not be completed ->'. $transaction_res["message"]	; 
 	
 	echo	'<h3>'.$error_message.'</h3>' ; 
-	
-	echo 	'<form method="post" action="do_transaction.php">'
+	echo 	'<form method="post" action="'.getPageURL($role).'">'
 		.	'<input type="hidden" name="dest_code" value="'.$dest_code.'">'
 		.	'<input type="hidden" name="amount" value="'.$amount.'">'
 		.	'<input type="hidden" name="description" value="'.$description.'">'
@@ -61,22 +83,6 @@ if ($transaction_res["result"] == true ){
 		.	'</form>' ;	
 }
 
-
-#var_dump($transaction_res) ; 
-
-
-#drawSingleTransactionTable($transaction_res) ; 
-
-
-#verifyTANCode($account_id, $tan_code) ;
-#ver
-# showing restult of verification 
-
-
-
-# Charge Extra fee if transfer is to outside Bank 
-
-# performing transaction
 
 
 

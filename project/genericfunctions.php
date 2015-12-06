c1f42855c4db37628ff2fcb3acfbacee2facd7c6
omar-osario <mahmoud.shadid@gmail.com>
2015-12-06 17:59:51 +0100

    Modifying San function return

--- project/genericfunctions.php
+++ project/genericfunctions.php
@@ -1,83 +1,96 @@
 <?php
 
-	define("SANITIZE_INT", 0);
-	define("SANITIZE_DOUBLE", 1);
-	define("SANITIZE_STRING_NAME", 2);
-	define("SANITIZE_STRING_EMAIL", 3);
-	define("SANITIZE_STRING_DESC", 4);
+define("SANITIZE_INT", 0);
+define("SANITIZE_DOUBLE", 1);
+define("SANITIZE_STRING_NAME", 2);
+define("SANITIZE_STRING_EMAIL", 3);
+define("SANITIZE_STRING_DESC", 4);
+
+function santize_input($input, $check_type = SANITIZE_STRING_DESC)
+{
+	$sanitized_input	= htmlspecialchars($input) ;
 	
-	function santize_input($input, $check_type = SANITIZE_STRING_DESC)
-	{
-		$sanitized_input	= htmlspecialchars($input) ;
-		
-		$sanitization	= array (
-			'result'	=> false, 
-			'input'		=> '',
-			'message'	=> 'Generic Sanitization failed',
-		) ;
-		
-		if ( $check_type == SANITIZE_INT ){
-			$sanitized_input	= filter_var($sanitized_input, FILTER_SANITIZE_NUMBER_INT);
-			if ( $sanitized_input !== false ){
-				$sanitization['result']		= true ; 
-				$sanitization['input']		= $sanitized_input ; 
-				$sanitization['message']	= 'Integer sanitizated succesfully' ; 
-			}
-			
-		} elseif ( $check_type == SANITIZE_DOUBLE ) {
-			$sanitized_input	= filter_var($sanitized_input, FILTER_SANITIZE_NUMBER_FLOAT,FILTER_FLAG_ALLOW_FRACTION );
-			# Removing multiple '.' in the number 
-			# check if double has a decimal point
-			
-			$first_dec	= strpos($sanitized_input,'.') ;
-			$last_dec	= strrpos($sanitized_input,'.') ;
-			$str_length	= strlen($sanitized_input) ;
-			
-			# check if double has more than 1 decimal point and remove if needed
-			if (  $first_dec !== false ){
-				if ( $first_dec != $last_dec ){
-					$first_part			= substr($sanitized_input,0,$first_dec+1) ; 
-					$last_part			= substr($sanitized_input,$first_dec+2) ;
-					$last_part			= str_replace('.','',$last_part) ;
-					$sanitized_input	= $first_part.$last_part ;
-				}
-			}
-			
-			if ( $sanitized_input !== false ){
-				$sanitization['result']		= true ; 
-				$sanitization['input']		= $sanitized_input ; 
-				$sanitization['message']	= 'Double sanitizated succesfully' ; 
-			}
-		} elseif ( $check_type == SANITIZE_STRING_NAME ) {
-			$sanitized_input	= filter_var($sanitized_input, FILTER_SANITIZE_STRING);
-			$sanitized_input 	= preg_replace( '/[^-a-zA-Z0-9\-]/', '', $sanitized_input );		if ( $sanitized_input !== false ){
-				$sanitization['result']		= true ; 
-				$sanitization['input']		= $sanitized_input ; 
-				$sanitization['message']	= 'Name sanitizated succesfully' ; 
-			}
-		} elseif ( $check_type == SANITIZE_STRING_EMAIL ) {
-			$sanitized_input	= filter_var($sanitized_input, FILTER_SANITIZE_EMAIL);
-			$sanitized_input 	= preg_replace( '/[^A-z0-9_\-\+\.\s]/', '', $sanitized_input );
-			if ( $sanitized_input !== false ){
-				$sanitization['result']		= true ; 
-				$sanitization['input']		= $sanitized_input ; 
-				$sanitization['message']	= 'Email sanitizated succesfully' ; 
-			}							
-		} elseif ($check_type == SANITIZE_STRING_DESC) {
-			$sanitized_input	= filter_var($sanitized_input, FILTER_SANITIZE_STRING);
-			if ( $sanitized_input !== false ){
-				$sanitization['result']		= true ; 
-				$sanitization['input']		= $sanitized_input ; 
-				$sanitization['message']	= 'String sanitizated succesfully' ; 
-			}										
+	$sanitization	= array (
+		'result'	=> false, 
+		'input'		=> false,
+		'message'	=> 'Generic Sanitization failed',
+	) ;
+	$error_msg_gen	= 'Please use appropriate values.' ;
+	
+	if ( $check_type == SANITIZE_INT ){
+		$sanitized_input	= filter_var($sanitized_input, FILTER_SANITIZE_NUMBER_INT);
+		if ( $sanitized_input !== false and !empty($sanitized_input)){
+			$sanitization['result']		= true ; 
+			$sanitization['input']		= $sanitized_input ; 
+			$sanitization['message']	= 'Integer sanitizated succesfully' ; 
 		} else {
+			$sanitization['message']	= 'Bad integer value, '.$error_msg_gen ; 		
+		}
+		
+	} elseif ( $check_type == SANITIZE_DOUBLE ) {
+		$sanitized_input	= filter_var($sanitized_input, FILTER_SANITIZE_NUMBER_FLOAT,FILTER_FLAG_ALLOW_FRACTION );
+		# Removing multiple '.' in the number 
+		# check if double has a decimal point
+		
+		$first_dec	= strpos($sanitized_input,'.') ;
+		$last_dec	= strrpos($sanitized_input,'.') ;
+		$str_length	= strlen($sanitized_input) ;
+		
+		# check if double has more than 1 decimal point and remove if needed
+		if (  $first_dec !== false ){
+			if ( $first_dec != $last_dec ){
+				$first_part			= substr($sanitized_input,0,$first_dec+1) ; 
+				$last_part			= substr($sanitized_input,$first_dec+2) ;
+				$last_part			= str_replace('.','',$last_part) ;
+				$sanitized_input	= $first_part.$last_part ;
+			}
+		}
+		
+		if ( $sanitized_input !== false and !empty($sanitized_input)){
+			$sanitization['result']		= true ; 
+			$sanitization['input']		= $sanitized_input ; 
+			$sanitization['message']	= 'Double sanitizated succesfully' ; 
+		} else {
+			$sanitization['message']	= 'Bad double value, '.$error_msg_gen ; 			
+		}
+	} elseif ( $check_type == SANITIZE_STRING_NAME ) {
+		$sanitized_input	= filter_var($sanitized_input, FILTER_SANITIZE_STRING);
+		$sanitized_input 	= preg_replace( '/[^-a-zA-Z0-9\-]/', '', $sanitized_input );
+		if ( $sanitized_input !== false and !empty($sanitized_input)){
+			$sanitization['result']		= true ; 
+			$sanitization['input']		= $sanitized_input ; 
+			$sanitization['message']	= 'Name sanitizated succesfully' ; 
+		} else {
+			$sanitization['message']	= 'Bad string(name) value, '.$error_msg_gen ;
+		}
+	} elseif ( $check_type == SANITIZE_STRING_EMAIL ) {
+		$sanitized_input	= filter_var($sanitized_input, FILTER_SANITIZE_EMAIL);
+		if ( $sanitized_input !== false and !empty($sanitized_input)){
+			$sanitization['result']		= true ; 
+			$sanitization['input']		= $sanitized_input ; 
+			$sanitization['message']	= 'Email sanitizated succesfully' ; 
+		}else{
+			$sanitization['message']	= 'Bad string(email) value, '.$error_msg_gen ;	
+		}							
+	} elseif ($check_type == SANITIZE_STRING_DESC) {
+		$sanitized_input	= filter_var($sanitized_input, FILTER_SANITIZE_STRING);
+		if ( $sanitized_input !== false and !empty($sanitized_input)){
 			$sanitization['result']		= true ; 
 			$sanitization['input']		= $sanitized_input ; 
 			$sanitization['message']	= 'String sanitizated succesfully' ; 
-		}
-		return $sanitization  ;
+		} else {
+			$sanitization['message']	= 'Bad string(generic) value, '.$error_msg_gen ;	
+		}										
+	} else {
+		$sanitization['message']	= 'Unknown sanitization error, '.$error_msg_gen ; 
 	}
-	
+	if ($sanitization['result']	== false ){
+		exit($sanitization['message']);
+	}
+			 
+	return $sanitization['input']  ;
+}
+		
 function base64ToBase10($base64Str) {
 	$alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 
